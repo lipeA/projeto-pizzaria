28debc04e04aece685621f5a49390018ac5aaa01
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2025-08-12 09:05:40 -0300

    feat:added error handling

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ const router = Express.Router();
 
 router.get("/teste", (req: Express.Request, res: Express.Response) => {
   return res.json({ ok: true });
+  //   throw new Error("error");
 });
 
 export = router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,29 @@
 import express = require("express");
+import "express-async-errors";
 import route = require("./routes");
+import cors = require("cors");
 
 type Request = express.Request;
 type Response = express.Response;
+type NextFunction = express.NextFunction;
 
 const app = express();
 app.use(express.json());
-app.use(route);
+app.use(cors());
 
+app.use(route);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error.",
+  });
+});
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000/teste");
 });
